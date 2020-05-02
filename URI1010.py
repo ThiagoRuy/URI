b29aa96f4babd4ed42cc439b9ebb861029b4da1b
ThiagoRuy <63025476+ThiagoRuy@users.noreply.github.com>
2020-05-02 15:07:17 -0300

    Create URI1010.py

---
+++ URI1010.py
@@ -0,0 +1,8 @@
+A = int(input()) #codigo peça 1 
+B = int(input()) #quantidade de peças 1
+C = float(input()) #valor unitario da peça 1 
+D = int(input()) #codigo peça 2
+E = int(input()) #quantidade de peças 2
+F = float(input()) #valor unitario da peça 2
+TOTAL = B*C + E*F
+print("VALOR A PAGAR: R$ {:.2f}".format(TOTAL)) 
